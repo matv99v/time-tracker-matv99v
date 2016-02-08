697ade4c31af43bc4e3156fcacff80f294740c1c
Vladimir Matveev <matv99v@gmail.com>
2016-02-08 19:12:39 +0200

    added user icon

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Button from 'react-bootstrap/lib/Button';
 
-export default class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <div>Do hometask</div>
-                <Button bsStyle="primary">Start timer</Button>
-            </div>
-        );
-    }
-}
+
+import Example from './Example.jsx';
+import UserAccountControls from './UserAccountControls.jsx';
+
+export default props => (
+    <div>
+        <h1>Time tracker</h1>
+        <UserAccountControls/>
+        <Example/>
+        <Button bsStyle="primary">Start timer</Button>
+    </div>
+);
---
+++ src/components/UserAccountControls.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Glyphicon from 'react-bootstrap/lib/Glyphicon';
+
+export default props => (
+    <div style={{ height: 120 }}>
+        <Glyphicon glyph="user"style={{ 'fontSize': '50px' }}/>
+    </div>
+);
