3256fcad4c443544144fbfd84e0c3e2af838d6d9
Vladimir Matveev <matv99v@gmail.com>
2016-04-13 12:03:27 +0300

    resolving router paths

--- src/main.js
+++ src/main.js
@@ -7,12 +7,16 @@ import ReactDOM     from 'react-dom';
 import App          from './components/App.jsx';
 import Registration from './components/Registration.jsx';
 
+import path from 'path';
 
+console.log(__dirname);
 
 ReactDOM.render((
         <Router history={browserHistory}>
-            <Route path='time-tracker-matv99v/public/' component={Registration} />
-            <Route path='time-tracker-matv99v/public/tracker' component={App} />
+            {/*<Route path='time-tracker-matv99v/public/' component={Registration} />
+            <Route path='time-tracker-matv99v/public/tracker' component={App} />*/}
+            <Route path={__dirname} component={Registration} />
+            <Route path={path.join(__dirname, '/tracker')} component={App} />
         </Router>
     ), document.getElementById('main-content')
 );
